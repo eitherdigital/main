72b6d4884725a39fe792421452e0092faa8bc1f5
kirillsaint <kirillsaint@wtfuck.tk>
2022-01-05 02:51:58 +0600

    hide tg tokens 2

--- app/Controllers/Http/AppController.js
+++ app/Controllers/Http/AppController.js
@@ -10,7 +10,7 @@ class AppController {
     var name = request.input('name')
     var email = request.input('email')
     var text = request.input('text')
-    axios.get(encodeURI(`https://api.telegram.org/bot${Env.get(TELEGRAM_TOKEN)}/sendMessage?chat_id=${Env.get(TELEGRAM_CHAT_ID)}&text=Новое сообщение:\n\nИмя: ${name}\nEmail: ${email}\nСообщение: ${text}`))
+    axios.get(encodeURI(`https://api.telegram.org/bot${Env.get('TELEGRAM_TOKEN')}/sendMessage?chat_id=${Env.get('TELEGRAM_CHAT_ID')}&text=Новое сообщение:\n\nИмя: ${name}\nEmail: ${email}\nСообщение: ${text}`))
     return response.redirect('/success')
   }
   async getReleases({ view }) {
